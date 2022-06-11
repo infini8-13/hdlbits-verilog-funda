1bc9af18a5db531d0a54a760e68fbd4c4e1916da
Saaswath <54203063+infini8-13@users.noreply.github.com>
2022-06-11 23:27:57 +0530

    Create Zero.v

---
+++ Getting_Started/Zero.v
@@ -0,0 +1,6 @@
+module top_module(
+    output zero
+);// Module body starts after semicolon
+//Left this blank since not assigning a value will lead to 0, in simulation
+  assign zero = 1'b0;
+endmodule
