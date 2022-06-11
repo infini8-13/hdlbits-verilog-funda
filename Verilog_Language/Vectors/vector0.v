76980da9add70af6c1e3e23e062fa339de85f59e
Saaswath <54203063+infini8-13@users.noreply.github.com>
2022-06-12 00:09:42 +0530

    Create vector0.v

---
+++ Verilog_Language/Vectors/vector0.v
@@ -0,0 +1,16 @@
+module top_module ( 
+    input wire [2:0] vec,
+    output wire [2:0] outv,
+    output wire o2,
+    output wire o1,
+    output wire o0  ); // Module body starts after module declaration
+
+    assign outv = vec;
+    assign {o2, o1, o0} = vec;
+    
+    //Instead of concat, the below statements could be used too
+    //assign o0 = vec[0];
+  	//assign o1 = vec[1];
+  	//assign o2 = vec[2];
+    
+endmodule
