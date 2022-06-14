5c876374e93453edc220b5236d4bf7d42f3f1ca4
Saaswath <54203063+infini8-13@users.noreply.github.com>
2022-06-14 14:22:04 +0530

    Create mux2to1v.v

---
+++ Circuits/Combinational_Logic/Multiplexers/mux2to1v.v
@@ -0,0 +1,10 @@
+// 100-bit wide, 2-to-1 multiplexer
+module top_module( 
+    input [99:0] a, b,
+    input sel,
+    output [99:0] out );
+    
+	assign out = sel ? b : a;
+    //assign out = (~{100{sel}} & a) | ({100{sel}} & b);
+    
+endmodule
