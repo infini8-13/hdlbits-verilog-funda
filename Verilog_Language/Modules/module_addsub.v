58ae6b3511635d8bc986e61868aa67b202eff3c3
Saaswath <54203063+infini8-13@users.noreply.github.com>
2022-06-23 23:12:58 +0530

    Update module_addsub.v

--- Verilog_Language/Modules/module_addsub.v
+++ Verilog_Language/Modules/module_addsub.v
@@ -10,7 +10,8 @@ module top_module(
     wire [15:0] sum1, sum2;
     add16 a1(a[15:0]  , b1[15:0] , sub , sum[15:0], cout);
     add16 a2(a[31:16] , b1[31:16], cout, sum[31:16],  c0);
-    assign b1 = sub ? (~b) : b;    
+    assign b1 = b^{32{sub}}; 
+    //assign b1 = sub ? (~b) : b;    
 
     
 endmodule
