32ec1610c60dca8770e0998a215250c8b61684f5
Saaswath <54203063+infini8-13@users.noreply.github.com>
2022-06-23 22:16:33 +0530

    Update module_fadd.v

--- Verilog_Language/Modules/module_fadd.v
+++ Verilog_Language/Modules/module_fadd.v
@@ -2,7 +2,7 @@ module top_module (
     input [31:0] a,
     input [31:0] b,
     output [31:0] sum
-);//
+);//ripple carry
 
     wire c1, c2;
     add16 a1( a[15:0] , b[15:0] , 1'b0, sum[15:0] , c1 );
