a00292597fa6124bac35cc6bf660865bdbe2850c
Saaswath <54203063+infini8-13@users.noreply.github.com>
2022-06-24 08:57:05 +0530

    Create always_if2.v

---
+++ Verilog_Language/Procedures/always_if2.v
@@ -0,0 +1,23 @@
+// if statement latches
+// synthesis verilog_input_version verilog_2001
+module top_module (
+    input      cpu_overheated,
+    output reg shut_off_computer,
+    input      arrived,
+    input      gas_tank_empty,
+    output reg keep_driving  ); //
+
+    always @(*) begin
+		shut_off_computer = 0;
+        if (cpu_overheated)
+           shut_off_computer = 1;
+    end
+
+    always @(*) begin
+        if (~arrived)
+           keep_driving = ~gas_tank_empty;
+        else
+           keep_driving = 0; 
+    end
+
+endmodule
