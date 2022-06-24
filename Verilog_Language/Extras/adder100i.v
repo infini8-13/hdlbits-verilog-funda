fd21e170c336b5e1214e3013fdf46dac17bb4fb2
Saaswath <54203063+infini8-13@users.noreply.github.com>
2022-06-24 15:10:27 +0530

    Update adder100i.v

--- Verilog_Language/Extras/adder100i.v
+++ Verilog_Language/Extras/adder100i.v
@@ -1,1 +1,25 @@
+module top_module( 
+    input [99:0] a, b,
+    input cin,
+    output [99:0] cout,
+    output [99:0] sum );
+    
+    genvar i;
+    fadd m1(a[0], b[0], cin, cout[0], sum[0]);
+    generate
+        for (i=1;i<100;i++) begin : fa
+            fadd m2(a[i], b[i], cout[i-1], cout[i], sum[i]);
+        end
+    endgenerate
+
+endmodule
+
+module fadd( 
+    input a, b, cin,
+    output cout, sum );
+
+    assign sum  = a^b^cin;
+    assign cout = a&b | b&cin | cin&a;
+    
+endmodule
 
