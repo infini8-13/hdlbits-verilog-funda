26c202f95346e0c49c3984216512280234fadf62
Saaswath <54203063+infini8-13@users.noreply.github.com>
2022-06-24 08:16:58 +0530

    Create alwaysblock1.v

---
+++ Verilog_Language/Procedures/alwaysblock1.v
@@ -0,0 +1,15 @@
+// synthesis verilog_input_version verilog_2001
+//AND gate with assign and always block(comb)
+module top_module(
+    input a, 
+    input b,
+    output wire out_assign,
+    output reg out_alwaysblock
+);
+
+    assign out_assign = a & b;
+    always @(*) out_alwaysblock = a & b;
+
+
+    
+endmodule
