a7b2e816807f0975abeaec5aac3db8f90a322297
Saaswath <54203063+infini8-13@users.noreply.github.com>
2022-07-05 20:14:17 +0530

    Create adder100b.v

---
+++ Circuits/Combinational_Logic/Arithmetic_Circuits/adder100b.v
@@ -0,0 +1,10 @@
+//100-bit binary adder
+module top_module( 
+    input [99:0] a, b,
+    input cin,
+    output cout,
+    output [99:0] sum );
+    
+    assign {cout,sum} = a + b + cin;
+
+endmodule
