7435486ea6f83e27d2df7434456d72bdb0108302
Saaswath <54203063+infini8-13@users.noreply.github.com>
2022-07-28 13:55:29 +0530

    Create count15.v

--- Circuits/Sequential_Logic/Counters/count15.v
+++ Circuits/Sequential_Logic/Counters/count15.v
@@ -1,1 +1,14 @@
+module top_module (
+    input clk,
+    input reset,      // Synchronous active-high reset
+    output [3:0] q);
+
+    always@(posedge clk) begin
+        if(reset==1 | q==4'hf)
+            q<=4'h0;
+        else
+            q<=q+4'h1;
+    end
+    
+endmodule
 
